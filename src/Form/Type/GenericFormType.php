8feafef6858a4935dd2f6269168d178a70767a9c
lucaszz <lucaszz111@gmail.com>
2015-11-22 20:01:48 +0100

    Generic form type introduced

---
+++ src/Form/Type/GenericFormType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Lucaszz\SymfonyGenericForm\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GenericFormType extends AbstractType
+{
+    /** @var string */
+    private $class;
+
+    /**
+     * @param string $class
+     */
+    public function __construct($class)
+    {
+        $this->class = $class;
+    }
+
+    /** {@inheritdoc} */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $reflection = new \ReflectionClass($this->class);
+
+        $resolver->setDefaults(
+            [
+                'compound'   => true,
+                'data_class' => $this->class,
+                'empty_data' => $reflection->newInstanceWithoutConstructor(),
+            ]
+        );
+    }
+
+    /** {@inheritdoc} */
+    public function getName()
+    {
+        return 'generic_form';
+    }
+}
