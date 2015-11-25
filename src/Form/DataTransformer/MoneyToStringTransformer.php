971ba7086908cc77e61754e1cb3f621d94edf06f
lucaszz <lucaszz111@gmail.com>
2015-11-25 18:39:22 +0100

    MoneyToStringTransformer introduced

---
+++ src/Form/DataTransformer/MoneyToStringTransformer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Lucaszz\SymfonyGenericForm\Form\DataTransformer;
+
+use Money\Currency;
+use Money\Money;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+class MoneyToStringTransformer implements DataTransformerInterface
+{
+    /** {@inheritdoc} */
+    public function transform($value)
+    {
+        if (null === $value) {
+            return '';
+        }
+
+        if (!$value instanceof Money) {
+            throw new TransformationFailedException('Expected a \Money\Money');
+        }
+
+        return sprintf('%.2f %s', (int) $value->getAmount() / 100.0, $value->getCurrency()->getName());
+    }
+
+    /** {@inheritdoc} */
+    public function reverseTransform($value)
+    {
+        try {
+            $parts = explode(' ', $value);
+
+            $amount   = isset($parts[0]) ? $parts[0] : null;
+            $currency = isset($parts[1]) ? $parts[1] : null;
+
+            $money = new Money((int) $amount * 100, new Currency($currency));
+        } catch (\Exception $e) {
+            throw new TransformationFailedException($e->getMessage());
+        }
+
+        return $money;
+    }
+}
---
+++ tests/Form/DataTransformer/MoneyToStringTransformerTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Lucaszz\SymfonyGenericForm\Tests\Form\DataTransformer;
+
+use Lucaszz\SymfonyGenericForm\Form\DataTransformer\MoneyToStringTransformer;
+use Lucaszz\SymfonyGenericForm\Tests\UnitTestCase;
+use Money\Money;
+
+class MoneyToStringTransformerTest extends UnitTestCase
+{
+    /** @var MoneyToStringTransformer */
+    private $transformer;
+
+    /** @test */
+    public function it_transforms_money_to_string()
+    {
+        $money = Money::USD(1000);
+
+        $transformed = $this->transformer->transform($money);
+
+        $this->assertEquals('10.00 USD', $transformed);
+    }
+
+    /** @test */
+    public function it_transforms_null_to_string()
+    {
+        $transformed = $this->transformer->transform(null);
+
+        $this->assertEquals('', $transformed);
+    }
+
+    /**
+     * @test
+     * @expectedException \Symfony\Component\Form\Exception\TransformationFailedException
+     */
+    public function it_can_not_transform_non_money()
+    {
+        $this->transformer->transform(new \stdClass());
+    }
+
+    /** @test */
+    public function it_reserve_transforms_string_to_money()
+    {
+        $transformed = $this->transformer->reverseTransform('10.00 USD');
+
+        $this->assertMoneyEquals(Money::USD(1000), $transformed);
+    }
+
+    /**
+     * @test
+     * @expectedException \Symfony\Component\Form\Exception\TransformationFailedException
+     */
+    public function it_can_not_reserve_transforms_invalid_uuid_string_to_uuid()
+    {
+        $this->transformer->reverseTransform('000 invalid-money');
+    }
+
+    /** {@inheritdoc} */
+    protected function setUp()
+    {
+        $this->transformer = new MoneyToStringTransformer();
+    }
+
+    /** {@inheritdoc} */
+    protected function tearDown()
+    {
+        $this->transformer = null;
+    }
+}
