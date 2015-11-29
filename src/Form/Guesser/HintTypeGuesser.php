e0bf0a3fdd35215d82ea01c7ae01db27e06f0a3e
lucaszz <lucaszz111@gmail.com>
2015-11-29 23:56:26 +0100

    GenericTypeGuesser removed

--- src/Form/Guesser/HintTypeGuesser.php
+++ src/Form/Guesser/HintTypeGuesser.php
@@ -2,8 +2,49 @@
 
 namespace Lucaszz\SymfonyFormGenerator\Form\Guesser;
 
-class HintTypeGuesser extends GenericTypeGuesser
+use Lucaszz\SymfonyFormGenerator\Form\Guesser\Resolver\TypeGuessResolver;
+use Symfony\Component\Form\FormTypeGuesserInterface;
+
+class HintTypeGuesser implements FormTypeGuesserInterface
 {
+    /** @var TypeGuessResolver */
+    private $resolver;
+
+    /**
+     * @param TypeGuessResolver $resolver
+     */
+    public function __construct(TypeGuessResolver $resolver)
+    {
+        $this->resolver = $resolver;
+    }
+
+    /** {@inheritdoc} */
+    public function guessType($class, $property)
+    {
+        $propertyType = $this->readPropertyType($class, $property);
+
+        if (null === $propertyType) {
+            return;
+        }
+
+        return $this->resolver->resolve($propertyType);
+    }
+
+    /** {@inheritdoc} */
+    public function guessRequired($class, $property)
+    {
+    }
+
+    /** {@inheritdoc} */
+    public function guessMaxLength($class, $property)
+    {
+    }
+
+    /** {@inheritdoc} */
+    public function guessPattern($class, $property)
+    {
+    }
+
     /** {@inheritdoc} */
     protected function readPropertyType($class, $property)
     {
--- src/Form/Guesser/PHPDocTypeGuesser.php
+++ src/Form/Guesser/PHPDocTypeGuesser.php
@@ -2,12 +2,52 @@
 
 namespace Lucaszz\SymfonyFormGenerator\Form\Guesser;
 
+use Lucaszz\SymfonyFormGenerator\Form\Guesser\Resolver\TypeGuessResolver;
 use phpDocumentor\Reflection\DocBlock;
 use phpDocumentor\Reflection\DocBlock\Tag\ParamTag;
 use phpDocumentor\Reflection\DocBlock\Tag\VarTag;
+use Symfony\Component\Form\FormTypeGuesserInterface;
 
-class PHPDocTypeGuesser extends GenericTypeGuesser
+class PHPDocTypeGuesser implements FormTypeGuesserInterface
 {
+    /** @var TypeGuessResolver */
+    private $resolver;
+
+    /**
+     * @param TypeGuessResolver $resolver
+     */
+    public function __construct(TypeGuessResolver $resolver)
+    {
+        $this->resolver = $resolver;
+    }
+
+    /** {@inheritdoc} */
+    public function guessType($class, $property)
+    {
+        $propertyType = $this->readPropertyType($class, $property);
+
+        if (null === $propertyType) {
+            return;
+        }
+
+        return $this->resolver->resolve($propertyType);
+    }
+
+    /** {@inheritdoc} */
+    public function guessRequired($class, $property)
+    {
+    }
+
+    /** {@inheritdoc} */
+    public function guessMaxLength($class, $property)
+    {
+    }
+
+    /** {@inheritdoc} */
+    public function guessPattern($class, $property)
+    {
+    }
+
     /** {@inheritdoc} */
     protected function readPropertyType($class, $property)
     {
