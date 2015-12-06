a743873a490e4051122c8e9f6f4fc72d28976ca1
lucaszz <lucaszz111@gmail.com>
2015-12-06 13:41:57 +0100

    ValidatorGuesser introduced

---
+++ src/Guesser/ValidatorGuesser.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Lucaszz\SymfonyFormGenerator\Guesser;
+
+use Lucaszz\SymfonyFormGenerator\Form\Guesser\Mapper\PropertyTypeToFormTypeMapper;
+use Symfony\Component\Form\Extension\Validator\ValidatorTypeGuesser;
+use Symfony\Component\Form\Guess\TypeGuess as SymfonyGuess;
+
+class ValidatorGuesser implements FormTypeGuesser
+{
+    /** @var ValidatorTypeGuesser */
+    private $symfonyGuesser;
+    /** @var PropertyTypeToFormTypeMapper */
+    private $mapper;
+
+    /**
+     * @param PropertyTypeToFormTypeMapper $mapper
+     * @param ValidatorTypeGuesser         $symfonyGuesser
+     */
+    public function __construct(PropertyTypeToFormTypeMapper $mapper, ValidatorTypeGuesser $symfonyGuesser)
+    {
+        $this->mapper         = $mapper;
+        $this->symfonyGuesser = $symfonyGuesser;
+    }
+
+    /** {@inheritdoc} */
+    public function guess($class, $property)
+    {
+        $symfonyGuess = $this->symfonyGuesser->guessType($class, $property);
+
+        if (null !== $symfonyGuess) {
+            return $this->transformGuess($symfonyGuess);
+        }
+    }
+
+    private function transformGuess(SymfonyGuess $symfonyGuess)
+    {
+        $formType = $symfonyGuess->getType();
+
+        if ($formType == 'datetime') {
+            $formType = 'generator_datetime';
+        }
+
+        return Guess::withDefaultOptions($formType);
+    }
+}
---
+++ tests/Guesser/ValidatorGuesserTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Lucaszz\SymfonyFormGenerator\Tests\Guesser;
+
+use Lucaszz\SymfonyFormGenerator\Form\Guesser\Mapper\PropertyTypeToFormTypeMapper;
+use Lucaszz\SymfonyFormGenerator\Guesser\Guess;
+use Lucaszz\SymfonyFormGenerator\Guesser\ValidatorGuesser;
+use Lucaszz\SymfonyFormGenerator\Tests\fixtures\ObjectWithAssertAnnotations;
+use Lucaszz\SymfonyFormGenerator\Tests\fixtures\ObjectWithoutMetadata;
+use Prophecy\Prophecy\ObjectProphecy;
+use Symfony\Component\Form\Extension\Validator\ValidatorTypeGuesser;
+use Symfony\Component\Form\Guess\TypeGuess;
+
+class ValidatorGuesserTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var ObjectProphecy|PropertyTypeToFormTypeMapper */
+    private $mapper;
+    /** @var ObjectProphecy|ValidatorTypeGuesser */
+    private $symfonyGuesser;
+    /** @var ValidatorGuesser */
+    private $guesser;
+
+    /** @test */
+    public function it_can_guess_basing_on_validator_metadata()
+    {
+        $this->symfonyGuesser->guessType(ObjectWithAssertAnnotations::class, 'propertyDateTime')->willReturn(new TypeGuess('datetime', [], TypeGuess::HIGH_CONFIDENCE));
+
+        $guess = $this->guesser->guess(ObjectWithAssertAnnotations::class, 'propertyDateTime');
+
+        $this->assertEquals(Guess::withDefaultOptions('generator_datetime'), $guess);
+    }
+
+    /** @test */
+    public function it_does_not_guess_when_no_validator_metadata()
+    {
+        $this->assertNull($this->guesser->guess(ObjectWithoutMetadata::class, 'propertyInteger'));
+    }
+
+    /** {@inheritdoc} */
+    protected function setUp()
+    {
+        $this->mapper         = $this->prophesize(PropertyTypeToFormTypeMapper::class);
+        $this->symfonyGuesser = $this->prophesize(ValidatorTypeGuesser::class);
+
+        $this->guesser = new ValidatorGuesser($this->mapper->reveal(), $this->symfonyGuesser->reveal());
+    }
+
+    /** {@inheritdoc} */
+    protected function tearDown()
+    {
+        $this->guesser = null;
+    }
+}
